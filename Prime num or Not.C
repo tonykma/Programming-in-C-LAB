c2f8c39f1a3b856a748af2afec617035a15dcef5
tonykma <96781650+tonykma@users.noreply.github.com>
2021-12-30 18:48:02 +0530

    Add files via upload

---
+++ Prime num or Not.C
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(void)
+{
+	int n,i,count=0;
+	printf("enter a number");
+	scanf("%d",&n);
+    for(i=1; i<=n;i++)
+	{
+		if(n%i==0)
+		{
+			count++;
+		}
+	}
+	if(count==2)
+	{
+		printf("%d is prime number ",n);
+	}
+	else
+    {
+		printf("%d is not a prime number",n);
+	}
+	return 0;
+}
