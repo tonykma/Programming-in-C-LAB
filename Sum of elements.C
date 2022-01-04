2258f99a3dc87b9870c76fbfb44cc1cd65dddd78
tonykma <96781650+tonykma@users.noreply.github.com>
2022-01-04 22:33:21 +0530

    Update Sum of elements.C

--- Sum of elements.C
+++ Sum of elements.C
@@ -3,14 +3,16 @@ int main(void)
 {
    int n,sum=0,a[50],i;
    printf("enter the size of array");
-   scanf("%d",&n);  printf("enter array elements");
+   scanf("%d",&n);  
+   printf("enter array elements");
   for(i=0;i<n;i++)
   {
 	  scanf("%d",&a[i]);
-  }   for(i=0;i<n;i++)
+  }  
+   for(i=0;i<n;i++)
   {
 	  sum=sum+a[i]; 
   } 
-printf("the sum of array element %d is",sum);
-return 0;
+  printf("the sum of array element %d is",sum);
+  return 0;
 }
