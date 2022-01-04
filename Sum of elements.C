39c3af76bb0468948c184a86896263e731c3f8f7
tonykma <96781650+tonykma@users.noreply.github.com>
2022-01-04 22:31:37 +0530

    Add files via upload

---
+++ Sum of elements.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void) 
+{
+   int n,sum=0,a[50],i;
+   printf("enter the size of array");
+   scanf("%d",&n);  printf("enter array elements");
+  for(i=0;i<n;i++)
+  {
+	  scanf("%d",&a[i]);
+  }   for(i=0;i<n;i++)
+  {
+	  sum=sum+a[i]; 
+  } 
+printf("the sum of array element %d is",sum);
+return 0;
+}
