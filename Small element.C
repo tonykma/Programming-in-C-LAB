bddf4edfe22873eeed2d3219790c69d0560b6d27
tonykma <96781650+tonykma@users.noreply.github.com>
2022-03-18 18:53:33 +0530

    Add files via upload

---
+++ Small element.C
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()
+{
+  int arr[10],n,i,small,pos,lar;
+  
+  printf("\n enter the number of elements in the array");
+  scanf("%d",&n);
+  printf("enter the elements");
+  for(i=0;i<n;i++)
+ { 
+    scanf("%d",&arr[i]);
+    small =arr[0];
+    pos=0;
+  }
+  for(i=1;i<n;i++)
+  {
+     if(arr[i]<small)
+     {
+       small=arr[i];
+       pos=i;
+     }
+  }
+	printf("\n the smallest element is %d", small);
+  printf("\n the position of the smallest element in the array is %d", pos);
+  return 0;
+}
