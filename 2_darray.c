329006765437747be54fe3e24aac8e7e02325b00
tonykma <96781650+tonykma@users.noreply.github.com>
2022-04-08 20:50:49 +0530

    Add files via upload

---
+++ 2_darray.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{ 
+   int arr[2][2]={1,2,3,4};
+   int i,j;
+   
+   for(i=0;i<2;i++)
+   {
+     for(j=0;j<2;j++)
+     {
+       printf("%d\t",arr[i][j]);
+     }
+     printf("\n");
+   }
+ return 0;
+}
