daa586da0bac645a08fbe66ac23aa66b308b1ddf
tonykma <96781650+tonykma@users.noreply.github.com>
2022-05-04 16:17:40 +0530

    Add files via upload

---
+++ Circular queue.C
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#define MAX 5
+int queue[MAX],rear=-1, front=-1;
+int insert();
+int delete();
+int display();
+int main()
+{
+   int ch;
+   
+   do
+   {
+      printf("\n *****MAIN MENU*****");
+      printf("\n 1.INSERT");
+      printf("\n 2.DELETE");
+      printf("\n 3.DISPLAY");
+      printf("\n 4.EXIT");
+      printf("\n enter your choice:");
+      scanf("%d",&ch);
+    switch(ch)
+    {
+       case 1:
+             insert();
+             break;
+       case 2:
+             delete();
+             break;
+       case 3:
+             display();
+             break;
+    }
+   } while(ch!=4);
+return 0;
+}
+int insert()
+{
+  int item;
+  if(front==0 && rear==MAX-1)
+  {
+     printf("OVERFLOW");
+  }
+  else
+  {
+     printf("enter the elements to insert:");
+     scanf("%d",&item);
+     if(front==-1&&rear==-1)
+     {
+        front=0;
+        rear=0;
+     }
+     else if(front!=0 && rear==MAX-1)
+     {
+         rear=0;
+     }
+     else
+     {
+        rear=rear+1;
+     }
+    queue[rear]=item;
+  }
+return 0;
+}
+int delete()
+{
+   int val;
+   if(front==-1)
+   printf("UNDERFLOW");
+   else
+   {
+     val=queue[front];
+     if(front==rear)
+     front=rear=-1;
+     else if(front==MAX-1)
+     front=0;
+     else
+     front=front+1;
+     printf("deleted element is %d",val);
+   }
+return 0;
+}
+int display()
+{
+  int i;
+  if(front==-1 && rear==-1)
+  printf("queue IS EMPTY");
+  else
+  {
+     printf("***QUEUE***\n");
+     if(front<rear)
+     {
+       for(i=front;i<=rear;i++)
+       printf("%d\t",queue[i]);
+     }
+     else
+     {
+       for(i=front;i<=MAX-1;i++)
+       printf("%d\t",queue[i]);
+       for(i=0;i<=rear;i++)
+       printf("%d\t",queue[i]);
+     }
+  }
+return 0;
+}
